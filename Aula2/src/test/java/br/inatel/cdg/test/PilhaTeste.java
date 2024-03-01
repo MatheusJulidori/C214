2cf83ea646fb8c034622ec31248163917fd8afc8
matheusjulidori <matheus_julidori@yahoo.com.br>
2024-03-01 20:30:37 -0300

    Aula 2 - Testes Unitários

---
+++ Aula2/src/test/java/br/inatel/cdg/test/PilhaTeste.java
@@ -0,0 +1,60 @@
+package br.inatel.cdg.test;
+
+import static org.junit.Assert.*;
+
+import java.util.EmptyStackException;
+
+import br.inatel.cdg.Pilha;
+
+import org.junit.Test;
+
+public class PilhaTeste {
+
+    @Test
+    public void testePilhaVazia() {
+        Pilha<Integer> pilhaInteiros = new Pilha<Integer>();
+        boolean vazia = pilhaInteiros.pilhaVazia();
+        assertTrue(vazia);
+    }
+
+    @Test
+    public void testePilhaPush() {
+        Pilha<Integer> pilhaInteiros = new Pilha<Integer>();
+        pilhaInteiros.push(13);
+        int tamanho = pilhaInteiros.size();
+        assertEquals(1, tamanho);
+    }
+
+    @Test
+    public void testePilhaValorElemento() {
+        Pilha<Integer> pilhaInteiros = new Pilha<Integer>();
+        pilhaInteiros.push(13);
+        int elemento = pilhaInteiros.pop();
+        assertEquals(13, elemento);
+    }
+
+    @Test
+    public void testePilhaPopSize() {
+        Pilha<Integer> pilhaInteiros = new Pilha<Integer>();
+        pilhaInteiros.push(13);
+        pilhaInteiros.pop();
+        int tamanho = pilhaInteiros.size();
+        assertEquals(0, tamanho);
+    }
+
+    @Test(expected = EmptyStackException.class)
+    public void testePilhaPopException() {
+        Pilha<Integer> pilhaInteiros = new Pilha<Integer>();
+        int retorno = pilhaInteiros.pop();
+    }
+
+    @Test
+    public void testePilhaPopRetorno() {
+        Pilha<Integer> pilhaInteiros = new Pilha<Integer>();
+        int valor = 13;
+        pilhaInteiros.push(valor);
+
+        int retorno = pilhaInteiros.pop();
+        assertEquals(valor, retorno);
+    }
+}
